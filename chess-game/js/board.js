1e0e84727f21403ff83e3e4c94bcbbaf70e665b7
Дышленко Александр <devilxhant@bk.ru>
2026-02-27 19:44:13 +0500

    Update board.js

--- chess-game/js/board.js
+++ chess-game/js/board.js
@@ -1,4 +1,217 @@
-// Класс доски
+// Класс для управления шахматной доской
+class Board {
+    constructor() {
+        this.cells = this.initializeBoard();
+        this.selectedPiece = null;
+        this.validMoves = [];
+    }
+
+    // Инициализация пустой доски
+    initializeBoard() {
+        const board = [];
+        for (let row = 0; row < 8; row++) {
+            board[row] = [];
+            for (let col = 0; col < 8; col++) {
+                board[row][col] = null;
+            }
+        }
+        return board;
+    }
+
+    // Установка начальной позиции
+    setupInitialPosition() {
+        // Белые фигуры (1-я и 2-я горизонтали)
+        this.cells[7][0] = new Rook('white', { row: 7, col: 0 });
+        this.cells[7][1] = new Knight('white', { row: 7, col: 1 });
+        this.cells[7][2] = new Bishop('white', { row: 7, col: 2 });
+        this.cells[7][3] = new Queen('white', { row: 7, col: 3 });
+        this.cells[7][4] = new King('white', { row: 7, col: 4 });
+        this.cells[7][5] = new Bishop('white', { row: 7, col: 5 });
+        this.cells[7][6] = new Knight('white', { row: 7, col: 6 });
+        this.cells[7][7] = new Rook('white', { row: 7, col: 7 });
+
+        for (let col = 0; col < 8; col++) {
+            this.cells[6][col] = new Pawn('white', { row: 6, col });
+        }
+
+        // Черные фигуры (7-я и 8-я горизонтали)
+        this.cells[0][0] = new Rook('black', { row: 0, col: 0 });
+        this.cells[0][1] = new Knight('black', { row: 0, col: 1 });
+        this.cells[0][2] = new Bishop('black', { row: 0, col: 2 });
+        this.cells[0][3] = new Queen('black', { row: 0, col: 3 });
+        this.cells[0][4] = new King('black', { row: 0, col: 4 });
+        this.cells[0][5] = new Bishop('black', { row: 0, col: 5 });
+        this.cells[0][6] = new Knight('black', { row: 0, col: 6 });
+        this.cells[0][7] = new Rook('black', { row: 0, col: 7 });
+
+        for (let col = 0; col < 8; col++) {
+            this.cells[1][col] = new Pawn('black', { row: 1, col });
+        }
+    }
+
+    // Получение фигуры по координатам
+    getPiece(row, col) {
+        if (!PositionUtils.isValidPosition(row, col)) return null;
+        return this.cells[row][col];
+    }
+
+    // Установка фигуры на доску
+    setPiece(row, col, piece) {
+        if (!PositionUtils.isValidPosition(row, col)) return false;
+        this.cells[row][col] = piece;
+        if (piece) {
+            piece.position = { row, col };
+        }
+        return true;
+    }
+
+    // Перемещение фигуры
+    movePiece(fromRow, fromCol, toRow, toCol) {
+        const piece = this.getPiece(fromRow, fromCol);
+        if (!piece) return null;
+
+        const capturedPiece = this.getPiece(toRow, toCol);
+        
+        // Перемещаем фигуру
+        this.setPiece(toRow, toCol, piece);
+        this.setPiece(fromRow, fromCol, null);
+        
+        // Отмечаем, что фигура двигалась
+        piece.hasMoved = true;
+
+        // TODO: Превращение пешки (опционально)
+
+        return capturedPiece;
+    }
+
+    // Получение всех легальных ходов для фигуры (с учетом шаха)
+    getLegalMoves(row, col) {
+        const piece = this.getPiece(row, col);
+        if (!piece) return [];
+
+        const allMoves = piece.getValidMoves(this.cells);
+        
+        // Фильтруем ходы, которые оставляют короля под шахом
+        const legalMoves = allMoves.filter(move => {
+            return !this.wouldBeInCheck(piece, move.row, move.col);
+        });
+
+        return legalMoves;
+    }
+
+    // Проверка, будет ли король под шахом после хода
+    wouldBeInCheck(piece, toRow, toCol) {
+        const { row, col } = piece.position;
+        
+        // Сохраняем текущее состояние
+        const targetPiece = this.getPiece(toRow, toCol);
+        
+        // Выполняем ход на копии доски
+        const boardCopy = new Board();
+        boardCopy.cells = PositionUtils.deepCopyBoard(this.cells);
+        
+        // Перемещаем фигуру
+        boardCopy.setPiece(toRow, toCol, boardCopy.getPiece(row, col));
+        boardCopy.setPiece(row, col, null);
+        
+        // Находим короля текущего игрока
+        const king = this.findKing(boardCopy, piece.color);
+        if (!king) return true;
+        
+        // Проверяем, находится ли король под атакой
+        return this.isSquareAttacked(boardCopy, king.position.row, king.position.col, piece.color);
+    }
+
+    // Поиск короля на доске
+    findKing(board, color) {
+        for (let row = 0; row < 8; row++) {
+            for (let col = 0; col < 8; col++) {
+                const piece = board.cells[row][col];
+                if (piece && piece.type === 'king' && piece.color === color) {
+                    return piece;
+                }
+            }
+        }
+        return null;
+    }
+
+    // Проверка, атакована ли клетка фигурами противника
+    isSquareAttacked(board, row, col, defendingColor) {
+        for (let r = 0; r < 8; r++) {
+            for (let c = 0; c < 8; c++) {
+                const piece = board.cells[r][c];
+                if (piece && piece.color !== defendingColor) {
+                    const moves = piece.getValidMoves(board.cells);
+                    if (moves.some(move => move.row === row && move.col === col)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    // Проверка, находится ли король текущего игрока под шахом
+    isInCheck(color) {
+        const king = this.findKing(this, color);
+        if (!king) return false;
+        
+        return this.isSquareAttacked(this, king.position.row, king.position.col, color);
+    }
+
+    // Проверка на мат
+    isCheckmate(color) {
+        if (!this.isInCheck(color)) return false;
+
+        // Проверяем, есть ли у игрока легальные ходы
+        for (let row = 0; row < 8; row++) {
+            for (let col = 0; col < 8; col++) {
+                const piece = this.cells[row][col];
+                if (piece && piece.color === color) {
+                    const legalMoves = this.getLegalMoves(row, col);
+                    if (legalMoves.length > 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    // Проверка на пат
+    isStalemate(color) {
+        if (this.isInCheck(color)) return false;
+
+        // Проверяем, есть ли у игрока легальные ходы
+        for (let row = 0; row < 8; row++) {
+            for (let col = 0; col < 8; col++) {
+                const piece = this.cells[row][col];
+                if (piece && piece.color === color) {
+                    const legalMoves = this.getLegalMoves(row, col);
+                    if (legalMoves.length > 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    // Очистка выделения
+    clearSelection() {
+        this.selectedPiece = null;
+        this.validMoves = [];
+    }
+
+    // Клонирование доски
+    clone() {
+        const newBoard = new Board();
+        newBoard.cells = PositionUtils.deepCopyBoard(this.cells);
+        return newBoard;
+    }
+}
+
+window.Board = Board;// Класс доски
 class Board {
     constructor() {
         this.cells = Array(8).fill().map(() => Array(8).fill(null));
